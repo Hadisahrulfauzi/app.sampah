521b2c579159d4470c446d3a376f9653f50fba07
Hadisahrulfauzi <147998079+Hadisahrulfauzi@users.noreply.github.com>
2024-12-15 19:57:20 +0700

    Update sampah.py

--- sampah.py
+++ sampah.py
@@ -69,15 +69,12 @@ else:
     if menu == "Beranda":
         st.markdown("""
         Sampah merupakan masalah lingkungan yang semakin kompleks akibat pertumbuhan populasi dan aktivitas industri. Pengelolaan sampah yang buruk dapat menyebabkan pencemaran dan membahayakan kesehatan. Solusi yang diusulkan adalah penggunaan teknologi Convolutional Neural Network (CNN) untuk mengklasifikasikan sampah secara otomatis melalui gambar. Sistem ini akan membantu masyarakat memisahkan sampah dengan lebih akurat dan memberikan panduan tentang cara mendaur ulang atau membuang sampah dengan benar. Dengan demikian, sistem ini diharapkan meningkatkan tingkat daur ulang, mengurangi beban TPA, dan mendukung ekonomi sirkular serta pengelolaan sampah berkelanjutan.
-        
-        Kelompok 4
-        Muhammad Ridwan Wibisono (211351098)
-        Hadi Sahrul Fauzi (211351060)
-        Mochammad Revan B (211351084)
-        Abriel Salsabina P.Y (211351001)
-        
-
         """, unsafe_allow_html=True)
+        st.markdown(""" Kelompok 4  """, unsafe_allow_html=True)
+        st.markdown(""" Muhammad Ridwan Wibisono (211351098)""", unsafe_allow_html=True)
+        st.markdown(""" Hadi Sahrul Fauzi (211351060)""", unsafe_allow_html=True)
+        st.markdown(""" Mochammad Revan B (211351084)""", unsafe_allow_html=True)
+        st.markdown(""" Abriel Salsabina P.Y (211351001)""", unsafe_allow_html=True)
 
     elif menu == "Kamera":
         # Pilihan untuk mengambil gambar menggunakan kamera
